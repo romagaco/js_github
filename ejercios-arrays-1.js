3fd4a4b8b7cbacc87414400db9e4507cebcc07fb
roberto <roberdex26@gmail.com>
2025-04-29 11:47:05 -0600

    making updates, resolving exercise 1

--- ejercios-arrays-1.js
+++ ejercios-arrays-1.js
@@ -63,22 +63,16 @@ Pista: usar Math.abs
 
 */
 
-// function vAbsoluto(arr) {
-//   let newArray = [];
-//   for (let i = 0; i < arr.length; i++){
-//     newArray.push(Math.abs(arr[i]));
-//   }
-//   return newArray;
-// }
 
-// function vAbsoluto(arr) {
-//   return arr.map(e => Math.abs(e));
-// }
+function vabsoluto(arr) {
+  let newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArray.push(Math.abs(arr[i]));
+}
+return newArray; }
 
-
-// const numbers = [1, 4, -5, -20, 8, 9, 3];
-// console.log(numbers);
-// console.log(vAbsoluto(numbers));
+const numbers = [1, 4, 5, -20, 8 , 9, 3]
+console.log(vabsoluto(numbers));
 
 
 /* Ejercicio 2
