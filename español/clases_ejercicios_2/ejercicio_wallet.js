2773a6916335903ae75f1d9c4924f267ee045de8
roberto <roberdex26@gmail.com>
2025-05-11 11:19:10 -0600

    end of wallet
    we finished with the functions, needed to fix a parameter.

--- español/clases_ejercicios_2/ejercicio_wallet.js
+++ español/clases_ejercicios_2/ejercicio_wallet.js
@@ -5,12 +5,14 @@ class Movimiento {
     cantidad;
     fecha;
     categoria;
+    tipo;
 
-    constructor(desc, cantidad, fecha, categoria) {
+    constructor(desc, cantidad, fecha, tipo, categoria) {
         this.descripcion = desc;
         this.cantidad = cantidad;
         this.fecha = fecha;
         this.categoria = categoria;
+        this.tipo = tipo;
     }
 }
 
@@ -28,7 +30,7 @@ class Billetera {
     // creamos un getter para los movimientos
     agregarMovimiento(movi) {
         this.#operaciones.push(movi);
-        if (movi.categoria == "Gasto") {
+        if (movi.tipo == "Gasto") {
             this.#monto = this.#monto - movi.cantidad;
         } else {
             this.#monto = this.#monto + movi.cantidad;
@@ -44,15 +46,47 @@ class Billetera {
     // ganancias por mes
     obtenerGananciaPorMes(mes){
         return this.#operaciones.filter(operacion => operacion.fecha.getMonth() === mes &&
-            operacion.categoria == "Ganancia");
+            operacion.tipo == "Ganancia");
     }
 
     // gastos por mes
-     obtenerGastoPorMes(mes){
+    obtenerGastoPorMes(mes){
         return this.#operaciones.filter(operacion => operacion.fecha.getMonth() === mes &&
-            operacion.categoria == "Gasto");
+            operacion.tipo == "Gasto");
     }
 
+
+
+    obtenerGastosPorCategoria(cat) {
+        return this.#operaciones.filter(movimiento => movimiento.tipo === "Gasto" && 
+            movimiento.categoria === cat);
+    }
+
+    obtenerGananciasPorCategoria(cat) {
+        return this.#operaciones.filter(movimiento => movimiento.tipo === "Ganancia" && 
+            movimiento.categoria === cat);
+    }
+
+
+    totalPormes(mes) {
+        return this.#operaciones
+        .filter(movimiento => movimiento.fecha.getMonth() === mes)
+        .reduce(
+            (total, movimiento)  => {
+                if(movimiento.tipo === "Gasto") {
+                    total = total - movimiento.cantidad
+                } else {
+                    total = total + movimiento.cantidad
+                }
+                return total;
+            }, 0
+        );
+    }
+
+
+    obtenerMonto(){
+        return this.#monto;
+    }
 }
 
 // probando las funciones de la billetera
@@ -62,9 +96,9 @@ let m0 = new Movimiento(
     "ingreso de nomina",
     3000,
     new Date(2025, 1, 1, 0, 0, 0, 0),
-    "Ganancia"
+    "Ganancia",
+    "Nomina"
 );
-
 cartera.agregarMovimiento(m0);
 
 
@@ -72,11 +106,29 @@ let m1 = new Movimiento(
     "gastos de supermercado",
     150,
     new Date(2025, 2, 2, 0, 0, 0, 0),
-    "Gasto"
+    "Gasto",
+    "Pan"
 );
-
 cartera.agregarMovimiento(m1);
 
 
-console.log(cartera.obtenergastosPorMes(1)); // muestra los movimientos del mes de febrero 
-console.log(cartera.obtenergastosPorMes(2)); // muestra los movimientos del mes de marzo
+let m2 = new Movimiento(
+    "gastos de supermercado",
+    450,
+    new Date(2025, 2, 7, 0, 0, 0, 0),
+    "Gasto",
+    "Leche"
+);
+cartera.agregarMovimiento(m2);
+
+console.log(cartera.obtenerMoviminetosPorMes(1)); // muestra los movimientos del mes de febrero 
+console.log(cartera.obtenerMoviminetosPorMes(2)); // muestra los movimientos del mes de marzo
+
+console.log(cartera.obtenerGastosPorCategoria("Pan"));
+console.log(cartera.obtenerGananciasPorCategoria("Nomina"));
+
+console.log(cartera.totalPormes(2)); // muestra el total de los movimientos del mes de marzo
+// como solo tenemos gastos el total es negativo
+
+console.log(cartera.obtenerMonto()); // muestra el monto total de la billetera
+
