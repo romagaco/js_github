b26d04c9a843294d807565fcb764eb65ea041208
roberto <roberdex26@gmail.com>
2025-06-05 18:45:43 -0600

    05/06/25
    05/06/25

--- español/clase_selectores/ejerciciios_selectores.js
+++ español/clase_selectores/ejerciciios_selectores.js
@@ -1,135 +1,41 @@
-console.log("ejercicio tele");
-
-class television {
-    #encendido = false;
-    #canal = 0;
-    #canales;
-    #volumen = 0;
-    #marca;
+console.log("ejercicio de selectores");
 
 
-    constructor(marca, canales){
-        this.#marca = marca;
-        this.#canales = canales;
-    }
-
-    encerder() {
-        this.#encendido = true;
-    }
-
-    apagar() {
-        this.#encendido = false;
-    }
-
-    siguienteCanal() {
-        if (this.#encendido) {
-            if (this.#canal === 100) {
-                this.#canal = 0;
-            } else {
-                this.#canal = this.#canal + 1;
-            }
-        } else {
-            console.log("El televisor no esta encedido, no se puede cambiar de canal");
-        }
-    }
-
-    canalAnterior() {
-        if (this.#encendido) {
-            if (this.#canal === 0) {
-                this.#canal = 100;
-            } else {
-                this.#canal = this.#canal - 1;
-            }
-        } else {
-            console.log("El televisor no esta encedido, no se puede cambiar de canal");
-        }
-    }
-
-    cambiarCanal(canal) {
-        if (this.#encendido) {
-            if (canal <=100 && canal >= 0) {
-                this.#canal = canal;
-            } else {
-                console.log("no posees ese canal en tu paquete, contacta con tu proveedor de cable");
-            } 
-         } else {
-             console.log("El televisor no esta encedido, no se puede cambiar de canal");
-         }
-    }
-
-    subirVolumen() {
-        if (this.#encendido) {
-            if (this.#volumen === 100) {
-                console.log("El volumen ya esta al maximo, no se puede subir mas");
-            } else {
-                this.#volumen = this.#volumen + 2;
-            }
-         } else {
-             console.log("El televisor no esta encedido, no se puede subir volumen");
-         }
-    }
-
-    bajarVolumen() {
-        if (this.#encendido) {
-            if (this.#volumen === 0) {
-                console.log("El volumen ya esta al minimo, no se puede bajar mas")
-            } else {
-                this.#volumen = this.#volumen - 2;
-            }
-         } else {
-             console.log("El televisor no esta encedido, no se puede subir volumen");
-         }
-    }
-
-    toString(){
-        return `Televisor: ${this.#marca}, canales: ${this.#canales}, Canal actual: ${this.#canal}, Volumen actual: ${this.#volumen}`;
-    }
-}
+document.addEventListener("DOMContentLoaded", function() { 
+// 1. selleciona el primer "p" de la pagina y modifica el texto a "Hola Mundo"
+let primerParrafo = document.getElementsByTagName("p")[0];
+primerParrafo.textContent = "Hola Mundo";
 
 
-let tele =  new television("Samsung", 100);
-console.log(tele.toString());
+// 2. selecciona el segundo enlace de primer p de la pagina y cambia su hrs por google.com
+let segundoParrafo = document.getElementsByTagName("p")[1];
+enlaces = segundoParrafo = document.getElementsByTagName("a");
+enlaces[2].setAttribute("href", "http://www.google.com");
 
-tele.cambiarCanal(97);  // no nos permite cambiar de canal porque no esta encendido
-console.log(tele.toString());
+//3. selleciiona el penultimo hijo de una etiqueta con el ID "mi lista"
+let milista = document.getElementById("milista");
+let hijos = milista.children;
+let penultimo = hijos(hijois.length - 2);
 
-tele.encerder(); // encendemos el televisor
-console.log(tele.toString());
-
-tele.cambiarCanal(97);  // cambiamos de canal
-console.log(tele.toString());
-
-tele.siguienteCanal(); // cambiamos de canal
-console.log(tele.toString());
-
-tele.siguienteCanal();
-tele.siguienteCanal();
-tele.siguienteCanal();
-tele.siguienteCanal(); // damos toda la vuelta para comprobar que este no va mas haya de los canales 
-console.log(tele.toString());
+//4. 
 
 
-tele.cambiarCanal(207);  // comprobamos que no se puede cambiar a un canal que no existe
-
-tele.subirVolumen(); // subimos el volumen
-console.log(tele.toString());
-
-tele.subirVolumen(); 
-tele.subirVolumen(); 
-tele.subirVolumen(); 
-tele.subirVolumen(); 
-tele.subirVolumen(); 
-console.log(tele.toString());
 
 
-tele.bajarVolumen(); // bajamos el volumen
-console.log(tele.toString());
-
-tele.bajarVolumen();
-tele.bajarVolumen();
-tele.bajarVolumen();
-console.log(tele.toString());
+});
 
 
-tele.apagar(); // apagamos el televisor
-console.log(tele.toString());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
