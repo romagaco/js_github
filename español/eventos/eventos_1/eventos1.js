bc121366a4131df0f9b0dfcedc4e3feeedff9b39
roberto <roberdex26@gmail.com>
2025-06-10 21:47:49 -0600

    10/06/25
    10/06/25

--- español/eventos/eventos_1/eventos1.js
+++ español/eventos/eventos_1/eventos1.js
@@ -8,6 +8,10 @@ let primerForm = document.forms[0]; // Accede al primer formulario del documento
 buttonSubmit.addEventListener("click", function(event) {
   event.preventDefault(); // Evita comportamiento por defecto del botón
   console.log("Boton de enviar pulsado");
+  // comprobamos que el email contiene un "@"
+  if (primerForm.elements[0].value.includes("@")) {
+    primerForm.submit();
+  } else { alert("Debe de añadir un @ al email"); }
 });
 
 buttonReset.addEventListener("click", function(event) {
@@ -15,3 +19,4 @@ buttonReset.addEventListener("click", function(event) {
   console.log("Boton de reset pulsado");
   primerForm.reset(); // Resetea el formulario
 });
+
